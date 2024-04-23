8248531de20e95a33a85b6f38337084969246f92
David Lynch <davite3@protonmail.com>
2024-04-23 11:16:08 +0100

    tried adding mesh

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -8,11 +8,22 @@ extends Node3D
 @export var multiplier = 1
 
 func _process(delta):
+	var gizmo: EditorNode3DGizmo = null
+	var cube: Mesh = null
+	cube.size = Vector3(base_size, base_size, base_size)
+	
+	gizmo.add_mesh(cube, null, Transform3D(
+		Basis(
+			Vector3(0,0,1), 
+			Vector3(0,0,0), 
+			Vector3(0,0,0)
+		),global_transform.origin))
 	pass
 
 func _ready():
 	if not Engine.is_editor_hint():		
 		pass
+	
 	
 
 
